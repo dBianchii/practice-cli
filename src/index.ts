8c2dfd86e1b29f1ed66dc44b44697eed73af1f54
dBianchii <gdbianchii@gmail.com>
2024-04-05 19:20:30 -0300

    mkay

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log("object");
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+	.argument("<string>", "string to log")
+	.action((message: string) => {
+		console.log(`Hello ${message}`);
+	}).description("It says heelo");
+
+program.command("add <numbers...>").action((numbers: string[]) => {
+	const sum = numbers.reduce((acc, num) => acc + parseInt(num), 0);
+	console.log(`Sum: ${sum}`);
+}).description("Add numbers");
+
+
+program.parse(process.argv);
